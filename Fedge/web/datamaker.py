10c89d34db6f34330a0a3a8d6cc39e5f40a445b0
Abasp <abaspoor@hotmail.com>
2023-09-20 18:49:33 +0330

    database and data maker ok for object 1 cabinet

--- Fedge/web/datamaker.py
+++ Fedge/web/datamaker.py
@@ -5,12 +5,12 @@ from django_apscheduler.jobstores import DjangoJobStore, register_events, regist
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-from web.mainmodels.cabinetlevel.cabinets import Cabinet
-from web.mainmodels.iolmodules.doorsensor import DoorsensorValue, DoorsensorDevice
-from web.mainmodels.iolmodules.led import LedDevice, LedValue
-from web.mainmodels.iolmodules.lock import LockactuatorDevice, LockactuatorValue
-from web.mainmodels.iolmodules.temperaturesensordevice import TemperaturesensorDevice, TemperaturesensorValue
-from web.mainmodels.modules.iolink import Iolink
+from .mainmodels.cabinetlevel.cabinets import Cabinet
+from .mainmodels.iolmodules.doorsensor import DoorsensorDevice, DoorsensorValue
+from .mainmodels.iolmodules.led import LedDevice, LedValue
+from .mainmodels.iolmodules.lock import LockactuatorDevice, LockactuatorValue
+from .mainmodels.iolmodules.temperaturesensordevice import TemperaturesensorDevice, TemperaturesensorValue
+from .mainmodels.modules.iolink import Iolink
 
 scheduler = BackgroundScheduler()
 scheduler.add_jobstore(DjangoJobStore(), "default")
@@ -24,19 +24,19 @@ def generate_random_float(start, end):
 @register_job(scheduler, IntervalTrigger(seconds=10), id='my_function', replace_existing=True)
 def my_function():
     _datetime = datetime.now()
-    cabinet = Cabinet.objects.get()
-    iolink = Iolink.objects.get(cabinet__in=cabinet)
+    cabinet = Cabinet.objects.get(profinet_name="K11111111111111")
+    iolink = Iolink.objects.get(cabinet=cabinet)
     # door sensor
-    doorsensor = DoorsensorDevice.objects.get(iolink__in=iolink)
+    doorsensor = DoorsensorDevice.objects.get(iolink=iolink)
     doorsensorvalue = DoorsensorValue.objects.create(doorsensordevice=doorsensor, value=True, timestamp=_datetime)
     # led device
-    leddevice = LedDevice.objects.get(iolink__in=iolink)
+    leddevice = LedDevice.objects.get(iolink=iolink)
     leddevicevalue = LedValue.objects.create(leddevice=leddevice, value=True, timestamp=_datetime)
     # lock actuator
-    lockactuator = LockactuatorDevice.objects.get(iolink__in=iolink)
+    lockactuator = LockactuatorDevice.objects.get(iolink=iolink)
     lockactuatorvalue = LockactuatorValue.objects.create(lockactuatordevice=lockactuator, value=True, timestamp=_datetime)
     # tempsensor
-    tempsensor = TemperaturesensorDevice.objects.get(iolink__in=iolink)
+    tempsensor = TemperaturesensorDevice.objects.get(iolink=iolink)
     tempsensorvalue = TemperaturesensorValue.objects.create(temperaturesensordevice=tempsensor, tempvalue=generate_random_float(24.5, 55.5), humidvalue=generate_random_float(20, 50), timestamp=_datetime)
-
-    print('successfully created')
+    print(iolink)
+    # print('successfully created')
