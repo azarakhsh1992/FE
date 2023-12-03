a37c7c1a619f25dba3119a17826c74cc08a35d57
Abasp <abaspoor@hotmail.com>
2023-12-03 20:56:37 +0330

    venv gitignore

--- Fedge/Fedge/settings.py
+++ Fedge/Fedge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3ao8j^=pbx#9k11i^ief0za2c7tw8(ze5-n^)@a98-#22ic4&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['home','127.0.0.1','localhost','192.168.1.1']
+ALLOWED_HOSTS = ['home','127.0.0.1','localhost','192.168.1.1','192.168.88.251','0.0.0.0']
 
 # Application definition
 
@@ -98,12 +98,16 @@ LOGGING = {
 
 
 WSGI_APPLICATION = 'Fedge.wsgi.application'
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "https://localhost:7005",
-    "https://localhost",
-    "https://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "https://localhost:7005",
+#     "https://localhost",
+#     "https://localhost:3000",
+#     "https://192.168.88.251:3000",
+#     "http://192.168.88.251:3000"
+# ]
 CORS_ALLOW_HEADERS = [
     'Content-Type',
     'Authorization'
